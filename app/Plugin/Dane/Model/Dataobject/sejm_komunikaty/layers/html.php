7ea42e82426bfe2a0a57b46706c9cf056aac149d
danielmacyszyn <daniel.macyszyn@epf.org.pl>
2015-11-18 22:37:03 +0100

    update

--- app/Plugin/Dane/Model/Dataobject/sejm_komunikaty/layers/html.php
+++ app/Plugin/Dane/Model/Dataobject/sejm_komunikaty/layers/html.php
@@ -1,5 +1,7 @@
 <?
 	
 	$data = @S3::getObject('resources', '/sejm_komunikaty/content/' . $id . '_modified.html');
+	if( !$data )
+		$data = @S3::getObject('resources', '/sejm_komunikaty/content/' . $id . '.html');
 	return @$data->body;
 	

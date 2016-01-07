84ec48ce40f684df2924a6fd35266e5b76b4449a
marek <marekbielecki93@gmail.com>
2016-01-07 15:59:17 +0100

    krakow ranking otwartosc

--- app/Plugin/Krakow/Controller/RadniRankingController.php
+++ app/Plugin/Krakow/Controller/RadniRankingController.php
@@ -94,7 +94,58 @@ class RadniRankingController extends AppController
 
             }
 
-        } elseif($type == '') {
+        } elseif($type == 'openness') {
+
+            $openness_points = array(
+                'email2' => 1,
+                'tel' => 5,
+                'blog' => 2,
+                'www' => 2,
+                'fb' => 2,
+                'twitter' => 2
+            );
+
+            $r = array();
+            $rows = $db->selectAssocs('
+                SELECT
+                    `pl_gminy_radni`.`id`,
+                    `pl_gminy_radni`.`nazwa`,
+                    `pl_gminy_radni`.`avatar_id`,
+                    `pl_gminy_radni`.`ranking_otwartosc_punkty`,
+                    `pl_gminy_radni`.`email2`,
+                    `pl_gminy_radni`.`tel`,
+                    `pl_gminy_radni`.`blog`,
+                    `pl_gminy_radni`.`www`,
+                    `pl_gminy_radni`.`fb`,
+                    `pl_gminy_radni`.`twitter`
+                FROM `pl_gminy_radni_krakow`
+                JOIN
+                  `pl_gminy_radni` ON
+                    `pl_gminy_radni`.`id` = `pl_gminy_radni_krakow`.`id`
+                ORDER BY `pl_gminy_radni`.`ranking_otwartosc_punkty`
+            ');
+
+            foreach($rows as $row) {
+
+                $types = array(
+                    '*' => $row['ranking_otwartosc_punkty']
+                );
+
+                foreach($openness_points as $k => $p) {
+                    if (!in_array($row[$k], array('', 'nie'))) {
+                        $types[$k] = $p;
+                    }
+                }
+
+                $r[] = array(
+                    'id' => $row['id'],
+                    'nazwa' => $row['nazwa'],
+                    'avatar_id' => $row['avatar_id'],
+                    'types' => $types
+                );
+            }
+
+            $results = $r;
 
         } else {
             throw new BadRequestException;

6735307ae8a126dc857c19bece97d6a00b7314f5
danielmacyszyn <daniel.macyszyn@epf.org.pl>
2016-06-28 08:01:48 +0200

    orzeczenia

--- app/Plugin/Dane/Model/Dataobject/sp_orzeczenia/layers/bloki.php
+++ app/Plugin/Dane/Model/Dataobject/sp_orzeczenia/layers/bloki.php
@@ -1,3 +1,22 @@
-<?php
-$data = $this->DB->query("SELECT id, tytul, wartosc FROM orzeczenia_bloki WHERE orzeczenie_id='$id' ORDER BY id ASC");
-return $data;
+<?
+	
+	$data = array();
+	
+	if(
+		( @$data = S3::getObject('resources', 'SP/orzeczenia/' . $id . '.json') ) && 
+		( @$data->body ) && 
+		( $data = @json_decode($data->body) )
+	) {
+		
+		
+		
+	} elseif(
+		$data = $this->DB->query("SELECT id, tytul, wartosc FROM orzeczenia_bloki WHERE orzeczenie_id='$id' ORDER BY id ASC")
+	) {
+		
+		foreach( $data as &$d )
+			$d = $d['orzeczenia_bloki'];
+		
+	}
+					
+	return $data;
